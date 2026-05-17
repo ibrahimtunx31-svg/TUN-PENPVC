fe0869edb84a4116927a5015b772277135363d30
ibrahimtunx31-svg <ibrahimtunx31@gmail.com>
2026-05-17 12:37:42 +0300

    google meta eklendi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 export const metadata = {
   title: "Tunç Pen PVC | Ankara Sineklik Cam Balkon Pimapen",
-  description:
-    "Ankara Mamak bölgesinde pimapen, sineklik ve cam balkon hizmetleri.",
+  description: "Ankara Mamak bölgesinde pimapen, sineklik ve cam balkon hizmetleri.",
+  verification: {
+    google: "N5aB_9GocYdkb-uw1w8Ji3CH3kWEj7_HI1BRE5hjUns",
+  },
 };
 
-export default function RootLayout ({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="tr">
       <body>{children}</body>
